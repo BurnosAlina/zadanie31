860dc02c0d7baf932f8da34db7dc8e087bb1ff54
Alina Burnos <burnosalina@gmail.com>
2023-05-26 16:04:11 +0200

    zadanie31 - poprawka

--- src/main/java/com/example/zadanie31/WeatherController.java
+++ src/main/java/com/example/zadanie31/WeatherController.java
@@ -3,7 +3,6 @@ package com.example.zadanie31;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -21,7 +20,7 @@ public class WeatherController {
         return "index";
     }
 
-    @PostMapping("/weather")
+    @GetMapping("/weather")
     String showWeather(@RequestParam String city, Model model) {
         try {
             WeatherResponseDto weather = weatherService.getWeather(city);
